06a7d7f3d7efe6b950367561a13d055e93b8d0ea
RagnaHugo <123764597+RagnaHugo@users.noreply.github.com>
2025-08-13 00:04:53 -0500

    added component OrderContents

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./index.css";
 import { menuItems } from "./data/db";
 import MenuItem from "./components/MenuItem";
 import useOrder from "./hooks/useOrder";
+import OrderContents from "./components/OrderContents";
 
 function App() {
-  const { addItem } = useOrder();
+  const { order, addItem } = useOrder();
 
   return (
     <>
@@ -23,8 +24,8 @@ function App() {
             ))}
           </div>
         </div>
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents order={order} />
         </div>
       </main>
     </>
